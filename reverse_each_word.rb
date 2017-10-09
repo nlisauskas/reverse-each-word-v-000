b77ccb5c41585cdcbb0a36dae872660fa8a52708
Nick Lisauskas <nlisausk@alumni.nd.edu>
2017-10-09 23:27:01 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,3 +5,10 @@ def reverse_each_word(sentence)
   end
   new_array.join(" ")
 end
+
+def reverse_each_word(sentence)
+  array = sentence.split(" ")
+  array.collect do |word|
+      word.reverse!
+  end
+end
